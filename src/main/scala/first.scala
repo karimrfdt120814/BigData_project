3f33102eda35d87051ddc824c42712f4fe9d5398
Karimulla <karimrfdt120814@gmail.com>
2024-10-05 00:39:23 +0530

    dummy

--- src/main/scala/first.scala
+++ src/main/scala/first.scala
@@ -1,6 +1,28 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+
 object first {
+
   def main(args:Array[String]):Unit= {
-    println("Hello Word")
+    //println("Hello Word")
+
+    val sc = new SparkContext("local[*]","demo")
+
+
+    // Create a Spark Session
+    val spark = SparkSession.builder()
+      .appName("MySparkApp") // Set your application name
+      .config("spark.some.config.option", "config-value") // Optional: Add any Spark configurations
+      .getOrCreate()
+
+    val rdd = sc.parallelize(1 to 100)
+
+    println("printing ********** :" +rdd.collect.foreach(println))
+    rdd.reduce(_+_)
+
+    //val df = spark.sql("select * from orders")
+    //df.printSchema()
+
   }
 }
 
