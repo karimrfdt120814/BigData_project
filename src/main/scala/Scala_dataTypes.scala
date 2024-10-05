1bc0da19e200211b553b10db4513d9c54219a230
Karimulla <karimrfdt120814@gmail.com>
2024-10-05 22:59:18 +0530

    datatypes

---
+++ src/main/scala/Scala_dataTypes.scala
@@ -0,0 +1,14 @@
+object Scala_dataTypes {
+
+  def main(args:Array[String]):Unit={
+    val number: Int = 42
+    val bigNumber:Long= 1234567890L
+    val pi: Double = 3.14159
+    val weight: Float = 65.5f
+    val isTrue: Boolean = true
+    val firstLetter:Char ='A'
+    val message: String = "Hello, Scala!"
+
+  }
+
+}
